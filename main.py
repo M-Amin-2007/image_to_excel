867060fcce392027848a24f923ecf8665fc7da04
Amin <mamp2007.mohammadi@gmail.com>
2025-09-09 09:19:10 +0330

    calculate average color of squares

--- main.py
+++ main.py
@@ -11,4 +11,21 @@ def image_on_excel(image_adress: str, excel_adress: str="", square_length: int=1
     if not excel_adress:
         excel_adress = re.sub(r"[/\\].+", f"/sample.xslx", image_adress)
     wb = Workbook()
-    img = Image.open("image_adress")
+    ws = wb.active
+    img = Image.open(image_adress)
+    w, h = img.size
+    for col in range(w // square_length):
+        for row in range(h// square_length):
+            sr = 0
+            sg = 0
+            sb = 0
+            for i in range((col - 1)*10, col * 10):
+                for j in range((row - 1)*10, row * 10):
+                    r, g, b = img.getpixel((i, j))
+                    sr += r
+                    sg += g
+                    sb += b
+            t = sr / square_length, sg / square_length, sb / square_length
+            print(t, (col, row))
+if __name__ == "__main__":
+    image_on_excel("sample.jpg", square_length=200)
