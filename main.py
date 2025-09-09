d67c43e8753fb73e451e7ec7af0fc8c3fc77e42f
Amin <mamp2007.mohammadi@gmail.com>
2025-09-09 10:22:54 +0330

    fix color format(hex) and fix cell adress(number to letters form)

--- main.py
+++ main.py
@@ -4,7 +4,16 @@ author: amin
 """
 import re
 from openpyxl import Workbook
+from openpyxl.styles import PatternFill
 from PIL import Image
+
+def number_to_letters(n):
+    result = ""
+    while n > 0:
+        n -= 1
+        result = chr(ord('A') + n % 26) + result
+        n //= 26
+    return result
 
 def image_on_excel(image_adress: str, excel_adress: str="", square_length: int=10):
     """it changes the cell colors acording to squares average color"""
@@ -25,7 +34,13 @@ def image_on_excel(image_adress: str, excel_adress: str="", square_length: int=1
                     sr += r
                     sg += g
                     sb += b
-            t = sr / square_length, sg / square_length, sb / square_length
-            print(t, (col, row))
+            rgb_color = sr // square_length, sg // square_length, sb // square_length
+            hex_color = f"{rgb_color[0]:02X}{rgb_color[1]:02X}{rgb_color[2]:02X}"
+            color = PatternFill(start_color=hex_color, end_color=hex_color, fill_type="solid")
+            print(col + 1, number_to_letters(col + 1))
+            cell_id = f"{number_to_letters(col + 1)}{row + 1}"
+            print(cell_id)
+            ws[cell_id].fill = color
+
 if __name__ == "__main__":
     image_on_excel("sample.jpg", square_length=200)
